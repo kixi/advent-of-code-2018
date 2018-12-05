1b7d24bf90de5d29ce17dd398aa726fc262cc5d7
kixi <kixi73@gmail.com>
2018-12-05 23:05:37 +0100

    day 5 part 1

---
+++ src/advent_of_code/day05.clj
@@ -0,0 +1,25 @@
+(ns advent-of-code.day05)
+
+(def input "dabAcCaCBAcCcaDA")
+(def input (slurp "src/advent_of_code/day05.input"))
+
+(defn react? [unit1 unit2]
+   (and (not= unit1 unit2)
+        (= (Character/toUpperCase unit1)
+           (Character/toUpperCase unit2))))
+
+(defn react-poly [[x & more :as all] unit]
+  (cond (nil? x) [unit]
+        (react? x unit) more
+        :else (cons unit all)))
+
+;; part 1
+(->>
+ (reduce react-poly
+         []
+         input)
+ (apply str)
+ count)
+
+
+              
